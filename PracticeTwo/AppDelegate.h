29705f4c0ca374bbfa72e9c96daae966ad3d390e
Partha Sarathi Reddy Bijjam <partha@ecoihub.com>
2019-02-12 15:07:53 +0530

    Initial Commit

---
+++ PracticeTwo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PracticeTwo
+//
+//  Created by Partha Sarathi Reddy Bijjam on 12/02/19.
+//  Copyright © 2019 Partha Sarathi Reddy Bijjam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PracticeTwo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PracticeTwo
+//
+//  Created by Partha Sarathi Reddy Bijjam on 12/02/19.
+//  Copyright © 2019 Partha Sarathi Reddy Bijjam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
